5baaa3743913a4e9b23eba8e43530d93f53bd5d5
ganeshkp <ganeshkumar.patil@gmail.com>
2023-01-08 20:13:29 +0000

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import re_path
+
+
+from .views import (
+    post_model_create_view,
+    post_model_detail_view,
+    post_model_delete_view,
+    post_model_list_view,
+    post_model_update_view
+)
+
+urlpatterns = [
+    re_path(r'^$', post_model_list_view, name='list'),
+    re_path(r'^create/$', post_model_create_view, name='create'),
+    re_path(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail'),
+    re_path(r'^(?P<id>\d+)/delete/$', post_model_delete_view, name='delete'),
+    re_path(r'^(?P<id>\d+)/edit/$', post_model_update_view, name='update'),
+    #url(r'^admin/', admin.site.urls),
+    #url(r'^$', home, name='home'),
+    #url(r'^redirect/$', redirect_somewhere, name='home')
+]
---
+++ djconcepts/urls.py
@@ -0,0 +1,31 @@
+"""djviews URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+# from django.conf.urls import url, include
+from django.urls import re_path, include
+from django.contrib import admin
+
+# from blog.views import post_model_list_view
+
+from .views import home, redirect_somewhere
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', home, name='home'),
+    re_path(r'^redirect/$', redirect_somewhere, name='home'),
+    # app controls all urls
+    re_path(r'^blog/', include('blog.urls')),
+    #url(r'^posts/$', post_model_list_view),
+]
